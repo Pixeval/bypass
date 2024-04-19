c3df43d0290454489e688ebca4946db3fbc372ac
Summpot <summpot@outlook.com>
2024-04-19 22:04:32 +0800

    Update

--- src/chrome_ssl_hook.rs
+++ src/chrome_ssl_hook.rs
@@ -44,7 +44,7 @@ unsafe extern "C" fn detour(s: *mut c_void, name: PCSTR) -> i32 {
     return result;
 }
 
-fn find_target() -> NativePointer {
+fn find_target() -> Option<NativePointer> {
     let chrome_main = loop {
         let chrome_main = Module::find_export_by_name(None, "ChromeMain");
         match chrome_main {
@@ -67,37 +67,46 @@ fn find_target() -> NativePointer {
     let result = memory_range.scan(&MatchPattern::from_string(
         "C7 44 24 20 ?? ?? ?? ?? 4C 8D 0D ?? ?? ?? ?? 31 F6 B9 10 00 00 00 31 D2 41 B8 D5 00 00 00",
     ).unwrap());
+    if result.is_empty() {
+        return None;
+    }
     let address = result[0].address;
     log::info!("found pattern at {:?}", address);
     let memory_range = MemoryRange::new(NativePointer(address.sub(200) as *mut c_void), 200);
     let result1 = memory_range.scan(&MatchPattern::from_string("41 56").unwrap());
     let result2 = memory_range.scan(&MatchPattern::from_string("56 57").unwrap());
+    if result1.is_empty() && result2.is_empty() {
+        return None;
+    }
     let address = if !result1.is_empty() {
         result1[0].address
     } else {
         result2[0].address
     };
     log::info!("found target at {:?}", address);
-    NativePointer(address as *mut c_void)
+    Some(NativePointer(address as *mut c_void))
 }
 
 pub fn install(auto_enable: bool) {
     eventlog::init("Pixeval.Bypass", log::Level::Trace).ok();
     let mut interceptr = Interceptor::obtain(&GUM);
-    let target = find_target();
-    interceptr.begin_transaction();
-    unsafe {
-        TARGET = Some(target);
-        *ORIGINAL.lock().unwrap().get_mut() = Some(transmute(
-            interceptr
-                .replace_fast(TARGET.unwrap(), NativePointer(detour as *mut c_void))
-                .unwrap()
-                .0,
-        ));
+    if let Some(target) = find_target() {
+        interceptr.begin_transaction();
+        unsafe {
+            TARGET = Some(target);
+            *ORIGINAL.lock().unwrap().get_mut() = Some(transmute(
+                interceptr
+                    .replace_fast(TARGET.unwrap(), NativePointer(detour as *mut c_void))
+                    .unwrap()
+                    .0,
+            ));
+        }
+        interceptr.end_transaction();
+        *ENABLED.lock().unwrap().get_mut() = auto_enable;
+        log::info!("chrome ssl hook installed");
+    } else {
+        log::error!("can't find target");
     }
-    interceptr.end_transaction();
-    *ENABLED.lock().unwrap().get_mut() = auto_enable;
-    log::info!("chrome ssl hook installed");
 }
 
 pub fn remove() {
